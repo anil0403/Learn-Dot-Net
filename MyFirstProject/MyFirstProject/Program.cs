11dfd655138e799d3043bf28031f40f308af0517
= <=>
2026-02-12 19:21:10 +0545

    For loop statement

--- MyFirstProject/MyFirstProject/Program.cs
+++ MyFirstProject/MyFirstProject/Program.cs
@@ -12,28 +12,12 @@ namespace MyFirstProject
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Enter day of the week");
-
-            int day = Convert.ToInt32(Console.ReadLine());
-
-            
-            switch(day)
+           for(int i =0; i < 10; i++)
             {
-                case 1: Console.WriteLine("Sunday");
-                    break;
-                case 2: Console.WriteLine("Monday");
-                    break;
-                case 3: Console.WriteLine("Tuesday");
-                    break;
-                case 4: Console.WriteLine("Wednesday");
-                    break;
-                case 5: Console.WriteLine("Thursday");
-                    break; 
-                case 6: Console.WriteLine("Friday");
-                    break;
-                default: Console.WriteLine("Saturday");
-                    break;
+                Console.WriteLine("Count = " + i);
             }
+            
+            
             
         }
     }
